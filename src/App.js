46127c97599c744b7ca113fd3d4c715c22ba3cd0
sevas <babagame7@gmail.com>
2023-01-06 23:58:02 +0900

    get pokemon date

--- src/App.js
+++ src/App.js
@@ -1,7 +1,33 @@
+import { useEffect, useState } from "react";
 import "./App.css";
+import { getAllPokemon } from "./utils/pokemon.js";
 
 function App() {
-  return <div className="App"></div>;
+  const initialURL = "https://pokeapi.co/api/v2/pokemon";
+
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchPokemonData = async () => {
+      // 全てのポケモンデータの取得
+      let res = await getAllPokemon(initialURL);
+      console.log(res);
+      setLoading(false);
+    };
+    fetchPokemonData();
+  }, []);
+
+  return (
+    <div className="App">
+      {loading ? (
+        <h1>ロード中...</h1>
+      ) : (
+        <>
+          <h1>ポケモンデータを取得しました</h1>
+        </>
+      )}
+    </div>
+  );
 }
 
 export default App;
